6ce40d0c5ca3293a50a37399a99fc35061dd667f
Lelikay <azabotkin04@gmail.com>
2024-07-31 12:02:40 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,6 @@
+HW = 12
+hours = 1.5
+course_name = "Python"
+HW_per_hour = hours / HW
+
+print ('Курс:', course_name,', всего задач:', HW, ', затрачено часов:', hours, 'среднее время выполнения ', HW_per_hour, ' часа')
