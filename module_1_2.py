5863bf58c38e2dccac45b7703f07fc8e25ffdb4b
Lelikay <azabotkin04@gmail.com>
2024-07-31 12:03:58 +0300

    Update module_1_2.py

--- module_1_2.py
+++ module_1_2.py
@@ -3,4 +3,4 @@ hours = 1.5
 course_name = "Python"
 HW_per_hour = hours / HW
 
-print ('Курс:', course_name,', всего задач:', HW, ', затрачено часов:', hours, 'среднее время выполнения ', HW_per_hour, ' часа')
+print ('Курс:', course_name,', всего задач:', HW, ', затрачено часов:', hours, ', среднее время выполнения ', HW_per_hour, ' часа')
